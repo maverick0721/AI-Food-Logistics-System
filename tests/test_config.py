12617e8bb24e1453d9fe25969a9794ef7905dad5
maverick0721 <shivang0721gupta@gmail.com>
2026-03-11 19:19:27 +0000

    Updated test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from utils.config import load_config
+
+ROOT = Path(__file__).resolve().parent.parent
 
 
 def test_config():
 
-    cfg = load_config("configs/settings.yaml")
+    cfg = load_config(ROOT / "configs/settings.yaml")
 
     assert "city" in cfg
