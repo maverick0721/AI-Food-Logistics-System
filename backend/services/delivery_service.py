ceac9a79e688937230d28587f50cdd697439f3a6
maverick0721 <shivang0721gupta@gmail.com>
2026-03-12 21:56:51 +0000

    Updated delivery_service.py so that every new order becomes a kafka event

--- backend/services/delivery_service.py
+++ backend/services/delivery_service.py
@@ -1,6 +1,24 @@
+from backend.streaming.kafka_producer import send_event
+from backend.streaming.topics import ORDER_CREATED
+
 def assign_driver(order_id):
 
     driver_id = order_id % 10
+
+    # Send event to Kafka
+    send_event(
+
+    ORDER_CREATED,
+
+    {
+
+        "order_id": db_order.id,
+        "user_id": order.user_id,
+        "restaurant_id": order.restaurant_id
+
+    }
+
+)
 
     return {
 
