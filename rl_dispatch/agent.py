ed290dbed49b3b98a3f5086fd37e88f5bf103057
maverick0721 <shivang0721gupta@gmail.com>
2026-03-12 13:25:37 +0000

    Defined dispatch Agent

---
+++ rl_dispatch/agent.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+
+class DispatchAgent(nn.Module):
+
+    def __init__(self, state_size, action_size):
+
+        super().__init__()
+
+        self.net = nn.Sequential(
+
+            nn.Linear(state_size, 64),
+            nn.ReLU(),
+
+            nn.Linear(64, 64),
+            nn.ReLU(),
+
+            nn.Linear(64, action_size)
+
+        )
+
+    def forward(self, x):
+
+        return self.net(x)
