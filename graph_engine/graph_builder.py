bdc2c8b7e20515f6652731760cde662b1af880b4
maverick0721 <shivang0721gupta@gmail.com>
2026-03-12 13:14:51 +0000

    Updated Graph Builder city boundary to train gnn model properly

--- graph_engine/graph_builder.py
+++ graph_engine/graph_builder.py
@@ -4,13 +4,18 @@ import networkx as nx
 
 class GraphBuilder:
 
-    def __init__(self, city):
+    def __init__(self, city, dist=5000):
 
         self.city = city
+        self.dist = dist
 
     def build(self):
 
-        graph = ox.graph_from_place(self.city, network_type="drive")
+        try:
+            graph = ox.graph_from_place(self.city, network_type="drive")
+        except TypeError:
+            center = ox.geocode(self.city)
+            graph = ox.graph_from_point(center, dist=self.dist, network_type="drive")
 
         graph = nx.convert_node_labels_to_integers(graph)
 
